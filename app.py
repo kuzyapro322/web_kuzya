3d7c1b03c73077f2dc1b0f984c3d52dc1ec3ba7e
kuzyapro322 <kir888555@mail.ru>
2026-02-17 06:30:07 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask, url_for, request
 from datetime import datetime
 import os
+from flask_sqlalchemy import SQLAlchemy
+from db import db
+from os import path
+from db.models import users
+from flask_login import LoginManager
 
 from lab1 import lab1
 from lab2 import lab2
@@ -9,12 +14,37 @@ from lab4 import lab4
 from lab5 import lab5
 from lab6 import lab6
 from lab7 import lab7
+from lab8 import lab8
 
 
 app=Flask(__name__)
 
-app.config['SECRET_KEY']=os.environ.get('SECRET_KEY', 'Секретно-секретный-секрет')
-app.config['DB_TYPE']=os.getenv('DB_TYPE', 'postgres')
+login_manager = LoginManager()
+login_manager.login_view = 'lab8.login'
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(login_id):
+    return users.query.get(int(login_id))
+
+app.config['SECRET_KEY']=os.environ.get('SECRET_KEY', 'Секретно-секретный-секрет').encode('utf-8')
+app.config['DB_TYPE'] = os.getenv('DB_TYPE', 'postgres')
+
+if app.config['DB_TYPE'] == 'postgres':
+    db_name = 'kirill_kuznetsov_orm'
+    db_user = 'kirill_kuznetsov_orm'
+    db_password = 'Kuzya_ORM_2026!'
+    host_ip = '127.0.0.1'
+    host_port = '5432'
+    app.config['SQLALCHEMY_DATABASE_URI'] = (
+        f'postgresql://{db_user}:{db_password}@{host_ip}:{host_port}/{db_name}'
+    )
+else:
+    dir_path = path.dirname(path.realpath(__file__))
+    db_path = path.join(dir_path, 'kirill_kuznetsov_orm.db')
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
+
+db.init_app(app)
 
 app.register_blueprint(lab1)
 app.register_blueprint(lab2)
@@ -23,7 +53,7 @@ app.register_blueprint(lab4)
 app.register_blueprint(lab5)
 app.register_blueprint(lab6)
 app.register_blueprint(lab7)
-
+app.register_blueprint(lab8)
 
 @app.route("/")
 def title_page():
@@ -35,7 +65,7 @@ def title_page():
     lab5_url = url_for("lab5.lab")
     lab6_url = url_for("lab6.lab")
     lab7_url = url_for("lab7.lab")
-
+    lab8_url = url_for("lab8.lab")
 
     return '''
 <!DOCTYPE html>
@@ -62,6 +92,7 @@ def title_page():
                 <li><a href="''' + lab5_url + '''">Лабораторная работа #5</a></li>
                 <li><a href="''' + lab6_url + '''">Лабораторная работа #6</a></li>
                 <li><a href="''' + lab7_url + '''">Лабораторная работа #7</a></li>
+                <li><a href="''' + lab8_url + '''">Лабораторная работа #8</a></li>
             </ul>
         </div>
     </main>
