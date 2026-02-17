8480fc9d25978fb810f72a5d87441804adfdaff7
kuzyapro322 <kir888555@mail.ru>
2026-02-17 08:11:32 +0700

    лаба 7

--- lab7.py
+++ lab7.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, request, abort, jsonify, current_app
 from datetime import datetime
 import psycopg2
-from psycopg2.extras import RealDictCursor
+from psycopg2.extras import RealDictCursor 
 import sqlite3
 from os import path
 
